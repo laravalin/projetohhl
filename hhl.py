d431a6b063de359aded7b7ec1dc6cb01a9dd3cd4
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2025-05-25 17:59:22 -0300

    ajusta scrapping

--- hhl.py
+++ hhl.py
@@ -3,7 +3,9 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Configuracoes
+from app.routes.scraper import scraper_bp
 
+app.register_blueprint(scraper_bp)
 
 @app.shell_context_processor
 def make_shell_context():
