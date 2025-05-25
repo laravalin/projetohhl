04fb011acb6f34b03dd9f4314f0d1ce11eb31acc
HenriqueSPiana <103382089+HenriqueSPiana@users.noreply.github.com>
2025-05-25 10:55:43 -0300

    insere alguns dados no BD e prepara ambiente de testes

--- hhl.py
+++ hhl.py
@@ -1,3 +1,10 @@
-#hhl
+#hhl ou app.py depende de como ta no documento apresentado pelo prof
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Configuracoes
 
-from app import app
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Configuracoes': Configuracoes}
